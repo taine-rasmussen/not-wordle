5eee471d25ffa0780f255e67f380a681e078524e
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-03-17 21:04:15 +1300

    created keyboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import "./App.css";
 
 function App() {
+  const keys = [
+    "Q",
+    "W",
+    "E",
+    "R",
+    "T",
+    "Y",
+    "U",
+    "I",
+    "O",
+    "P",
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "ENTER",
+    "Z",
+    "X",
+    "C",
+    "V",
+    "B",
+    "N",
+    "M",
+    "«",
+  ];
+
   return (
     <div className="App">
       <div className="game-container">
@@ -9,7 +40,11 @@ function App() {
         </div>
         <div className="message-container"></div>
         <div className="tile-container"></div>
-        <div className="key-container"></div>
+        <div className="key-container">
+          {keys.map((key) => (
+            <button key={key}>{key}</button>
+          ))}
+        </div>
       </div>
     </div>
   );

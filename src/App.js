8c82845d4e8cfae79d0779891cbb25ef6adaa01a
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-03-17 20:49:15 +1300

    basic styling / html markup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
-    </div>  
+      <div className="game-container">
+        <div className="title-container">
+          <h1>Wordle</h1>
+        </div>
+        <div className="message-container"></div>
+        <div className="tile-container"></div>
+        <div className="key-container"></div>
+      </div>
+    </div>
   );
 }
 

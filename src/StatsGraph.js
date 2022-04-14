eba0d637491c6ee3fe25dc7ed3fbd5b67a38a594
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-04-14 20:00:59 +1200

    set up example graph

--- src/StatsGraph.js
+++ src/StatsGraph.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -17,9 +18,45 @@ export const StatsGraph = (props) => {
     gameSession
   } = props
 
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const labels = ['One', 'Two', 'Three', 'Four', 'Five'];
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top'
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+  };
+
+
   return (
     <div className='graph-container'>
-
+      <Bar options={options} data={data} />
     </div>
   )
 }

c89f5071fca074716331944a6452fc9dd44857b1
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-04-14 20:09:54 +1200

    working basic graph

--- src/StatsGraph.js
+++ src/StatsGraph.js
@@ -27,32 +27,21 @@ export const StatsGraph = (props) => {
     Legend
   );
 
-  const labels = ['One', 'Two', 'Three', 'Four', 'Five'];
-
+  const labels = Object.keys(gameSession[0].totalAttemps);
   const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top'
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Bar Chart',
-      },
-    },
+
   };
 
   const data = {
     labels,
     datasets: [
       {
-        label: 'Dataset 1',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        label: 'Total attemps',
+        data: gameSession[0].totalAttemps,
+        backgroundColor: '#aeb8bd',
       },
     ],
   };
-
 
   return (
     <div className='graph-container'>
